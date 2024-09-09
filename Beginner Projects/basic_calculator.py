cfcde32d8c9417c1b87ed7c789e4c69de72035cb
Shreya Jamnadas <shreyajamnadas@gmail.com>
2024-09-09 17:03:35 -0400

    Renamed file

---
+++ Beginner Projects/basic_calculator.py
@@ -0,0 +1,68 @@
+# Shreya Jamnadas
+# September 8, 2024
+# Creating a Basic Calculator using user input, loops, and if statements
+# Next Steps: Creating a Basic Calculator using Functions
+
+# Creating do while loops for user input
+# asking for operation type 
+while True:
+    operationType = input("What is the operation you want to use? (ex: Addition, Subtraction, Multiplication, Division, Modulus): ")
+    if operationType.lower() == "addition" or operationType.lower() == "subtraction" or operationType.lower() == "multiplication" or operationType.lower() == "division" or operationType.lower() == "modulus":
+        break
+    else:
+        print("Invalid Answer!")
+        continue
+
+#asking for numbers
+numbers = [] #creating an empty array/list
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+#adding num1 and num2 into list numbers
+numbers.append(float(num1)) 
+numbers.append(float(num2))
+while True:
+    finish = input("Would you like to enter another number?: ")
+    if finish.lower() == "no":
+        break
+    elif finish.lower() == "yes":
+        num3 = input("Enter a number: ")
+        numbers.append(float(num3))
+        continue
+
+# Calculate values depending on the operation
+if operationType.lower() == "addition":
+    result = 0
+    for x in numbers: #for loops for arrays - according to python tho this is not an array it is a list (numbers) but can be considered as an array
+        result += x
+elif operationType.lower() == "multiplication":
+    result = 1
+    for x in numbers:
+        result *= x
+elif operationType.lower() == "subtraction":
+    result = numbers[0]
+    for x in numbers:
+        result -= x
+        if result == 0:
+            result = numbers[0]
+            continue
+elif operationType.lower() == "division":
+    result = numbers[0]
+    for x in numbers:
+        result /= x
+        if result == 1:
+            result = numbers[0]
+            continue
+elif  operationType.lower() == "modulus":
+    result = numbers[0]
+    counter = 0
+    for x in numbers:
+        # I still think there is an error here but I will come back to it
+        result %= x 
+        if result == 0 and counter < 1:
+            counter += 1
+            result = numbers[0]
+            continue
+        else:
+            break
+
+print("Answer: "+str(result))
