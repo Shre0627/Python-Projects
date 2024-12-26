7a621c6b97129e6e590eb7b5669a8de2751074cf
Shreya Jamnadas <shreyajamnadas@gmail.com>
2024-12-26 00:01:58 -0500

    optimizing code and adding match statement

--- Beginner Projects/basic_calculator.py
+++ Beginner Projects/basic_calculator.py
@@ -1,51 +1,49 @@
 # Shreya Jamnadas
 # September 8, 2024
-# Creating a Basic Calculator using user input, loops, if statements, functions, and arrays - this calculator only calculates with a single operator
+# Creating a Basic Calculator using user input, loops, if statements, functions, list, and match statements - this calculator only calculates with a single operator
 # Next Steps: Program the calculator to use multiple operators
 
 def calc(arr, type):
-    value = 0
-    if type == "+":
-        for x in arr:
-            value += x
-        return value
-    elif type == "-":
-        for x in arr:
-            firstEle = arr[0]
-            if x == firstEle:
-                value = x
-                continue
-            value -= x
-        return value
-    elif type == "*":
-        for x in arr:
+    match type:
+        case "+": 
+            value = 0
+            for x in arr:
+                value += x
+            return value
+        case "-": 
+            for x in arr:
+                firstEle = arr[0]
+                if x == firstEle:
+                    value = x
+                    continue
+                value -= x
+            return value
+        case "*":
             value = 1
-            value *= x
-        return value
-    elif type == "/":
-        for x in arr:
-            firstEle = arr[0]
-            if x == firstEle:
-                value= x
-                continue
-            value /= x
-        return value   
-    print("Result "+str(value))   
+            for x in arr:
+                value *= x
+            return value
+        case "/":
+            for x in arr:
+                firstEle = arr[0]
+                if x == firstEle:
+                    value = x
+                    continue
+                value /= x
+            return value  
 
 operationType = input("What operation would you like to use? Addition (+), Substraction (-), Multiplication (*), or Division (/) - use symbols: ")
+operationType = operationType.strip()
 while True:
     if operationType == "+" or operationType == "-" or operationType == "*" or operationType == "/":
         break
     else:
         print("Invalid Answer")
         operationType = input("What operation would you like to use? Addition (+), Substraction (-), Multiplication (*), or Division (/) - use symbols: ")
+        operationType = operationType.strip()
 
-calcQ = input("Input the numbers in a list (with spaces in between each number): ")
-numbers = [] # creating an empty array list
-
-# for loop to iterate through each element in String
-for x in calcQ:
-    if isinstance(x, (int, float)): # checking if x is a number or not
-        numbers.append(float(x)) 
-
-calc(numbers, operationType)
+calcQ = input("Input the numbers in a list (with space in between each number): ")
+numbers = calcQ.split(" ")
+numbers = [float(x) for x in numbers]
+answer = calc(numbers, operationType)
+print("Result: "+str(answer))
