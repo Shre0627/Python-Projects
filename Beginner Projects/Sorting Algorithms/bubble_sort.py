c87faaf401a03b5e8896fdf456da33c52ca75474
Shreya Jamnadas <shreyajamnadas@gmail.com>
2025-01-03 21:43:31 -0500

    Implemented Bubble Sort algorithm from scratch

---
+++ Beginner Projects/Sorting Algorithms/bubble_sort.py
@@ -0,0 +1,21 @@
+# Shreya Jamnadas
+# January 3, 2025
+# Coding the Bubble Sort Algorithm by Scratch using loops and nested loops, conditional statements, try and catch, and lists
+
+while True:
+    values = input("Enter numbers seperated by a comma: ")
+    numbers = values.split(",") # list of numbers
+    try:
+        numbers = [int(x) for x in numbers]
+        break
+    except Exception:
+        print("Invalid Inputs. Please input all integers!")
+
+for index in range(len(numbers)):
+    for index2 in range(index+1, len(numbers)):
+        if numbers[index] > numbers[index2]:
+                temp = numbers[index]
+                numbers[index] = numbers[index2]
+                numbers[index2] = temp
+
+print(numbers)
